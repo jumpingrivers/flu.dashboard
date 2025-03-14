ce1d9fd7023d560cc950dcd8b6d0cb507e67a39d
Keith Newman <keith@jumpingrivers.com>
2025-03-14 12:22:47 +0000

    feat: Add value boxes for change over time period.

--- R/mod_bed_usage.R
+++ R/mod_bed_usage.R
@@ -1,24 +1,30 @@
+#' Create a page for displaying bed usage
+#'
+#' Dashboard content for displaying summary information about bed-occupancy for a particular cause
+#' of illness.
+#'
+#' @param id Unique Shiny module ID to link the UI and Server components together.
+#' @param cause Display name of the illness that a patient occupies a bed for. Write in lower case
+#'   unless capitalisation is needed to denote an acronym of proper noun, since this will be
+#'   displayed as text in the middle of sentences where needed.
+#' @rdname bed_usage
 bed_usage_ui <- function(id,
                          cause) {
   ns <- shiny::NS(id)
   shiny::tagList(
     shiny::h1(cause),
-    bslib::layout_columns(
-      bslib::value_box(
-        title = "Number of Trusts",
-        value = shiny::textOutput(ns("number_of_filtered_trusts")),
-        showcase = bsicons::bs_icon("hospital")
-      ),
-      bslib::value_box(
-        title = "Current bed occupancy",
-        value = shiny::textOutput(ns("current_bed_occupancy")),
-        showcase = bsicons::bs_icon("person-bounding-box"),
-        shiny::p("Based on number of laboratory-confirmed cases")
-      )
-    )
+    value_box_row_ui(id = ns("value_boxes"), cause = cause)
   )
 }
 
+#' @rdname bed_usage
+#' @param board_ref A pointer to a Posit Connect board created by [pins::board_connect] or an
+#'   alternative location.
+#' @param pin_name Name of the pin on the Posit Connect board containing the bed usage data for this
+#'   particular illness. This data should have been pre-processed. If the data is in parquet format,
+#'   then the nanoparquet package is required.
+#' @param selected_trusts Character vector of the trust ID codes that the user has selected in the
+#'   filter.
 #' @importFrom rlang .data .env
 bed_usage_server <- function(id,
                              board_ref,
@@ -40,20 +46,9 @@ bed_usage_server <- function(id,
     }) |>
       shiny::bindEvent(selected_trusts(), ignoreNULL = TRUE, ignoreInit = TRUE)
 
-    ## Value boxes ----
-    output$number_of_filtered_trusts <- renderText(length(selected_trusts()))
-
-    output$current_bed_occupancy <- renderText({
-      current_usage <- filtered_data |>
-        purrr::chuck("full") |>
-        dplyr::slice_max(order_by = .data$date, n = 1, by = "code", with_ties = TRUE)
-      formatC(
-        sum(current_usage$occupancy, na.rm = TRUE),
-        format = "f",
-        big.mark = ",",
-        digits = 0L
-      )
-    })
+    value_box_row_server(id = "value_boxes",
+                         selected_trusts = selected_trusts,
+                         filtered_data = filtered_data)
 
   })
 }
